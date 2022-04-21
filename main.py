2948156748381a7f1362c4968b38b45dc6f38f07
LuisVillela <103969295+LuisVillela@users.noreply.github.com>
2022-04-20 23:05:06 -0600

    Como hacerlo...?

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("hello world")
 print("Que tal?")
 #comentario a medio codigo 
+
+#lo primero es hacer un cambio en el codigo, guardarlo, pasar a github y darle nombre al commit y darle a comit to main. luego pasas al historial t le das al cmoit y le das a push
